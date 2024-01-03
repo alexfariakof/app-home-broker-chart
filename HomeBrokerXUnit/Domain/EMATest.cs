895b0fec7819459c58733f8ebd639f701a41c445
Ribeiro <alexfariakof@gmail.com>
2024-01-03 11:01:46 -0300

    Implementação de Testes Unitários

---
+++ HomeBrokerXUnit/Domain/EMATest.cs
@@ -0,0 +1,39 @@
+﻿using Domain.Charts.ValueObject;
+namespace Domain;
+public class EMATest
+{
+    [Fact]
+    public void Should_Calculates_EMA_Correctly()
+    {
+        // Arrange
+        List<decimal> historyPriceData = new List<decimal> {
+            22.27m, 22.19m, 22.08m, 22.17m, 22.18m, 22.13m, 22.23m, 22.43m, 22.24m, 22.29m, 22.15m, 22.39m, 22.38m, 22.61m,23.36m, 
+            24.05m, 23.75m, 23.83m, 23.95m, 23.63m, 23.82m, 23.87m, 23.65m, 23.19m, 23.10m, 23.33m, 22.68m, 23.10m, 22.40m, 22.17m 
+        };
+
+        List<decimal> expectedEMA = new List<decimal> {
+            22.22m, 22.21m, 22.24m, 22.27m, 22.33m, 22.52m, 22.80m, 22.97m, 23.13m, 23.28m, 23.34m, 23.43m, 23.51m, 23.53m, 23.47m, 
+            23.40m, 23.39m, 23.26m, 23.23m, 23.08m, 22.92m
+        };
+
+        // Act
+        var ema = new EMA(historyPriceData, 10);
+
+        // Assert
+        for(int i=0;i < ema.Values.Count;i++)
+        {
+            Assert.Equal(expectedEMA[i], Math.Round(ema.Values[i], 2));
+        }        
+    }
+
+    [Fact]
+    public void Should_Throws_Erro_when_Calculates_EMA()
+    {
+        // Arrange
+        List<decimal> historyPriceData = new List<decimal> { 22.22m, 22.21m, 22.24m, 22.27m, };
+
+        // Act & Assert
+        var exception = Assert.Throws<ArgumentException>(() => new EMA(historyPriceData, 10));
+        Assert.Equal("Não há dados suficientes para gerar uma EMA.", exception.Message);
+    }
+}
---
+++ HomeBrokerXUnit/Domain/SMATest.cs
@@ -0,0 +1,34 @@
+﻿using Domain.Charts.ValueObject;
+namespace Domain;
+public class SMATest
+{
+    [Fact]
+    public void Should_Calculates_SMA_Correctly()
+    {
+        // Arrange
+        List<decimal> historyPriceData = new List<decimal> { 11, 12, 13, 14, 15, 16, 17 };
+
+        // Act
+        var sma = new SMA(historyPriceData, 5);
+
+        // Assert
+        Assert.Equal(13, sma.Values[0]);
+        Assert.Equal(14, sma.Values[1]);
+        Assert.Equal(15, sma.Values[2]);
+        Assert.Equal(12.4m, sma.Values[3]);
+        Assert.Equal(9.6m, sma.Values[4]);
+        Assert.Equal(6.6m, sma.Values[5]);
+        Assert.Equal(3.4m, sma.Values[6]);
+    }
+
+    [Fact]
+    public void Should_Throws_Erro_when_Calculates_SMA()
+    {
+        // Arrange
+        List<decimal> historyPriceData = new List<decimal> { 11, 12, 13, 14 };
+
+        // Act & Assert
+        var exception = Assert.Throws<ArgumentException>(() => new SMA(historyPriceData));
+        Assert.Equal("Não há dados suficiente para gerar uma SMA.", exception.Message);
+    }
+}
