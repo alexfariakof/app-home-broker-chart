634626a9f62de4fc8a2f38d5c34acf1bf408ce55
Ribeiro <alexfariakof@gmail.com>
2024-01-03 00:30:35 -0300

    Implementação da Função GetEMA do  Controller recebendo parâmetro de período de dias

--- webapi/Controllers/ChartHomeBrokerController.cs
+++ webapi/Controllers/ChartHomeBrokerController.cs
@@ -4,7 +4,6 @@ using Domain.Charts.ValueObject;
 using Microsoft.AspNetCore.Mvc;
 
 namespace webapi.Controllers;
-
 [ApiController]
 [Route("[controller]")]
 public class ChartHomeBrokerController : ControllerBase
@@ -16,22 +15,19 @@ public class ChartHomeBrokerController : ControllerBase
         _logger = logger;
         _homeBrokerBusiness = homeBrokerBusiness;
     }
-
     [HttpGet]
     public List<MagazineLuizaHistoryPrice> Get([FromQuery] DateTime StartDate, [FromQuery] DateTime EndDate)
     {
         var period = new Period(StartDate, EndDate);
         return _homeBrokerBusiness.GetHistoryData(period);
     }
-
     [HttpGet("GetSMA")]
     public SMA GetSMA()
     {
         return _homeBrokerBusiness.GetSMA();
     }
-
-    [HttpGet("GetEMA")]
-    public EMA GetEMA([FromQuery] int PeriodDays)
+    [HttpGet("GetEMA/{PeriodDays}")]
+    public EMA GetEMA([FromRoute] int PeriodDays)
     {
         return _homeBrokerBusiness.GetEMA(PeriodDays);
     }
