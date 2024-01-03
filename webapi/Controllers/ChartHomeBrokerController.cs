1489a701ff6ef298770e420a7eafa08a56a7ebed
Ribeiro <alexfariakof@gmail.com>
2024-01-03 11:01:00 -0300

    Refinamento da Classe Controller Remoção de Injeção não utilizada

--- webapi/Controllers/ChartHomeBrokerController.cs
+++ webapi/Controllers/ChartHomeBrokerController.cs
@@ -4,28 +4,30 @@ using Domain.Charts.ValueObject;
 using Microsoft.AspNetCore.Mvc;
 
 namespace webapi.Controllers;
+
 [ApiController]
 [Route("[controller]")]
 public class ChartHomeBrokerController : ControllerBase
 {
-    private readonly ILogger<ChartHomeBrokerController> _logger;
     private IHomeBrokerBusiness _homeBrokerBusiness;
-    public ChartHomeBrokerController(ILogger<ChartHomeBrokerController> logger, IHomeBrokerBusiness homeBrokerBusiness)
+    public ChartHomeBrokerController(IHomeBrokerBusiness homeBrokerBusiness)
     {
-        _logger = logger;
         _homeBrokerBusiness = homeBrokerBusiness;
     }
+
     [HttpGet]
     public List<MagazineLuizaHistoryPrice> Get([FromQuery] DateTime StartDate, [FromQuery] DateTime EndDate)
     {
         var period = new Period(StartDate, EndDate);
         return _homeBrokerBusiness.GetHistoryData(period);
     }
+
     [HttpGet("GetSMA")]
     public SMA GetSMA()
     {
         return _homeBrokerBusiness.GetSMA();
     }
+
     [HttpGet("GetEMA/{PeriodDays}")]
     public EMA GetEMA([FromRoute] int PeriodDays)
     {
