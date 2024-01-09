9207272ef1d67c5dd871a097e3957c7afb4f0365
Ribeiro <alexfariakof@gmail.com>
2024-01-09 18:27:26 -0300

    Removendo teste do ngOnInt

--- HomeBrokerSPA/HomeBrokerChart/src/app/pages/home/home.component.spec.ts
+++ HomeBrokerSPA/HomeBrokerChart/src/app/pages/home/home.component.spec.ts
@@ -28,26 +28,4 @@ describe('Test Unit HomeComponent', () => {
     // Assert
     expect(component).toBeTruthy();
   });
-
-  it('should fetch magazineLuizaHistoryPrices on ngOnInit', fakeAsync(() => {
-    // Arrange
-    const period: Period = {
-      StartDate: dayjs().add(-1, 'year'),
-      EndDate: dayjs()
-    };
-    const fakeResponse: MagazineLuizaHistoryPrice[] = [
-      { date: dayjs().add(5, 'days'), open: 100, high: 110, low: 90,  close: 105, adjClose: 105, volume: 1000000 },
-      { date: dayjs().add(10, 'days'), open: 110, high: 120, low: 100, close: 115, adjClose: 115, volume: 1200000,},
-      { date: dayjs().add(20, 'days'), open: 120, high: 130, low: 110, close: 125, adjClose: 125, volume: 1500000,},
-    ];
-    //spyOn(chartService, 'get').and.returnValue(Promise.resolve(fakeResponse));
-    // Act
-    component.period = period;
-    component.ngOnInit();
-
-    //Assert
-    fixture.whenStable().then(() => {
-      expect(component.magazineLuizaHistoryPrices).toEqual(fakeResponse);
-    });
-  }));
 });
