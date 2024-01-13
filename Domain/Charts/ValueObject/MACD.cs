78e22b568c9a63706155d6eaf72348ef1a526d72
Ribeiro <alexfariakof@gmail.com>
2024-01-13 03:50:27 -0300

    Create Unit Test MACD

--- Domain/Charts/ValueObject/MACD.cs
+++ Domain/Charts/ValueObject/MACD.cs
@@ -1,12 +1,16 @@
-﻿namespace Domain.Charts.ValueObject;
+﻿using Domain.Charts.Agreggates;
+
+namespace Domain.Charts.ValueObject;
 public record MACD
 {
     public List<decimal> MACDLine { get; set; } = new List<decimal>();
     public List<decimal> Signal{ get; set; } = new List<decimal>();    
     public List<decimal> Histogram { get; set; } = new List<decimal>();
 
-    public MACD(List<decimal> historyPriceData)
+    public MACD(List<MagazineLuizaHistoryPrice> historyData)
     {
-
+        MACDLine = historyData.Select(price => price.Close).ToList();
+        Signal = historyData.Select(price => price.Close).ToList();
+        Histogram = historyData.Select(price => price.Close).ToList();
     }
 }
