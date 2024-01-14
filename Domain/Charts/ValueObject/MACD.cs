ac3e21b6222fa74f219030ccc1df7e5f6b376dec
Ribeiro <alexfariakof@gmail.com>
2024-01-14 17:20:03 -0300

    Atualização Branch main com Staging #13

---
+++ Domain/Charts/ValueObject/MACD.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Charts.Agreggates;
+
+namespace Domain.Charts.ValueObject;
+public record MACD
+{
+    public List<decimal> MACDLine { get; set; } = new List<decimal>();
+    public List<decimal> Signal{ get; set; } = new List<decimal>();    
+    public List<decimal> Histogram { get; set; } = new List<decimal>();
+
+    public MACD(List<MagazineLuizaHistoryPrice> historyData)
+    {
+        MACDLine = historyData.Select(price => price.Close).ToList();
+        Signal = historyData.Select(price => price.Close).ToList();
+        Histogram = historyData.Select(price => price.Close).ToList();
+    }
+}
---
+++ HomeBrokerXUnit/Domain/MACDTest.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Charts.ValueObject;
+using HomeBrokerXUnit.Faker;
+
+namespace Domain;
+public class MACDTest
+{
+    [Fact]
+    public void Should_Create_MACD_Correctly()
+    {
+        // Arrange
+        var faker = MagazineLuizaHistoryPriceFaker.GetListFaker(10);
+
+        // Act
+        var macd = new MACD(faker);
+
+        // Assert
+        Assert.NotNull(macd);
+        Assert.NotNull(macd.MACDLine);
+        Assert.NotNull(macd.Signal);
+        Assert.NotNull(macd.Histogram);
+    }
+}
