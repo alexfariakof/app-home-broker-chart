ea7ab684419c3f4d90773911e7dac950dc806bee
Ribeiro <alexfariakof@gmail.com>
2024-01-28 21:27:16 -0300

    Tratamento de Erro para Data Inicial e DataFinal

--- HomeBrokerSPA/Controllers/ChartHomeBrokerController.cs
+++ HomeBrokerSPA/Controllers/ChartHomeBrokerController.cs
@@ -10,38 +10,76 @@ namespace HomeBrokerSPA.Controllers;
 public class ChartHomeBrokerController : ControllerBase
 {
     private readonly IHomeBrokerBusiness _homeBrokerBusiness;
-    
+
     public ChartHomeBrokerController(IHomeBrokerBusiness homeBrokerBusiness)
     {
         _homeBrokerBusiness = homeBrokerBusiness;
     }
 
     [HttpGet("{StartDate}/{EndDate}")]
-    public List<MagazineLuizaHistoryPrice> Get([FromRoute] DateTime StartDate, [FromRoute] DateTime EndDate)
-    {
-        var period = new Period(StartDate, EndDate);
-        var result = _homeBrokerBusiness.GetHistoryData(period);
-        return result;
+    [ProducesResponseType((200), Type = typeof(List<MagazineLuizaHistoryPrice>))]
+    [ProducesResponseType((400), Type = typeof(String))]
+    public IActionResult Get([FromRoute] DateTime StartDate, [FromRoute] DateTime EndDate)
+    {        
+        try
+        {
+            var period = new Period(StartDate, EndDate);
+            var result = _homeBrokerBusiness.GetHistoryData(period);
+            return Ok(result);
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(new { message = "Erro desconhecido, tente novamente mais tarde."} );
+        }
     }
 
     [HttpGet("GetSMA/{StartDate}/{EndDate}")]
-    public Sma GetSMA([FromRoute] DateTime StartDate, [FromRoute] DateTime EndDate)
-    {
-        var period = new Period(StartDate, EndDate);
-        return _homeBrokerBusiness.GetSMA(period);
+    [ProducesResponseType((200), Type = typeof(Sma))]
+    [ProducesResponseType((400), Type = typeof(String))]
+    public IActionResult GetSMA([FromRoute] DateTime StartDate, [FromRoute] DateTime EndDate)
+    {        
+        try
+        {
+            var period = new Period(StartDate, EndDate);
+            var result = _homeBrokerBusiness.GetSMA(period);
+            return Ok(result);
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(new { message = ex.Message });
+        }
     }
 
     [HttpGet("GetEMA/{PeriodDays}/{StartDate}/{EndDate}")]
-    public Ema GetEMA([FromRoute] int PeriodDays, [FromRoute] DateTime StartDate, [FromRoute] DateTime EndDate)
-    {
-        var period = new Period(StartDate, EndDate);
-        return _homeBrokerBusiness.GetEMA(PeriodDays, period);
+    [ProducesResponseType((200), Type = typeof(Ema))]
+    [ProducesResponseType((400), Type = typeof(String))]
+    public IActionResult GetEMA([FromRoute] int PeriodDays, [FromRoute] DateTime StartDate, [FromRoute] DateTime EndDate)
+    {        
+        try
+        {
+            var period = new Period(StartDate, EndDate);
+            return Ok(_homeBrokerBusiness.GetEMA(PeriodDays, period));
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(new { message = ex.Message });
+        }        
     }
 
     [HttpGet("GetMACD/{StartDate}/{EndDate}")]
-    public MACD GetMACD([FromRoute] DateTime StartDate, [FromRoute] DateTime EndDate)
+    [ProducesResponseType((200), Type = typeof(MACD))]
+    [ProducesResponseType((400), Type = typeof(String))]
+    public IActionResult GetMACD([FromRoute] DateTime StartDate, [FromRoute] DateTime EndDate)
     {
-        var period = new Period(StartDate, EndDate);
-        return _homeBrokerBusiness.GetMACD(period);
+        
+        try
+        {
+            var period = new Period(StartDate, EndDate);
+            return Ok(_homeBrokerBusiness.GetMACD(period));
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(new { message = ex.Message });
+        }
     }
 }
