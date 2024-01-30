09e3a3049260a4f697e9b9706333fb804f5f54a0
Ribeiro <alexfariakof@gmail.com>
2024-01-30 16:18:36 -0300

    Configuração de Execução dos Testes Unitários

--- HomeBrokerXUnit/coverage_report.sh
+++ HomeBrokerXUnit/coverage_report.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
+#clear
 projectTestPath=$(pwd)
 projectPath=$(realpath ..)
+projectAngular="$projectPath/HomeBrokerSPA/HomeBrokerChart"
 sourceDirs="$projectPath/Business;$projectPath/Domain;$projectPath/Repository;$projectPath/HomeBrokerSPA;$projectPath/HomeBrokerSPA/HomeBrokerChart"
 reportPath="$projectTestPath/TestResults"
 coverageXmlPath="$projectTestPath/TestResults/coveragereport"
 
-# Executa Testes Unitários sem restore e build e gera o relatório de cobertura
+# Excuta Teste Unitarios sem restore e build e gera o relatório de cobertura do Backend
 dotnet test  ./HomeBrokerXUnit.csproj --results-directory "$reportPath" -p:CollectCoverage=true -p:CoverletOutputFormat=cobertura --collect:"XPlat Code Coverage;Format=opencover" --no-restore --no-build > /dev/null 2>&1
 reportgenerator -reports:$projectTestPath/coverage.cobertura.xml -targetdir:$coverageXmlPath/coveragereport -reporttypes:'Html;lcov;' 
+
+cd "$projectAngular"
+
+# Verifica se a pasta node_modules existe e, se não existir, executa npm install
+if [ ! -d "node_modules" ]; then
+    npm install
+fi
+
+# Executa Testes Unitários e gera o relatório de cobertura do Frontend
+npm run test:coverage
+
+cd "$projectTestPath
