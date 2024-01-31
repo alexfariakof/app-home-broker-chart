108dbddef219a0c8b81748529e9875dca6bad050
Ribeiro <alexfariakof@gmail.com>
2024-01-31 12:06:28 -0300

    Implementação de Validação dos períodos no Frontend

--- HomeBrokerSPA/HomeBrokerChart/src/app/shared/components/period-filter/period.filter.component.ts
+++ HomeBrokerSPA/HomeBrokerChart/src/app/shared/components/period-filter/period.filter.component.ts
@@ -1,16 +1,18 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MdbFormsModule } from "mdb-angular-ui-kit/forms";
 import { PeriodStartDateObservable } from './../../observables/period/period.startDate.observable';
 import { PeriodEndDateObservable } from '../../observables';
 import { CustomValidators } from '../../validators';
-import { Dayjs } from 'dayjs';
+
 @Component({
   selector: 'app-period-filter',
   standalone: true,
   templateUrl: './period.filter.component.html',
   styleUrls: ['./period.filter.component.css'],
-  imports: [CommonModule, FormsModule, ReactiveFormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, MdbFormsModule ],
+
 })
 export class PeriodFilterComponent implements OnInit {
   periodForm: FormGroup;
@@ -20,15 +22,15 @@ export class PeriodFilterComponent implements OnInit {
     public obsStartDate: PeriodStartDateObservable,
     public obsEndDate: PeriodEndDateObservable) {
 
-    this.periodForm = this.builder.group(
-      {
-        filterStart: new FormControl(obsStartDate.startDate, Validators.required),
-        filterEnd: new FormControl(obsEndDate.endDate, Validators.required),
-      },
-      {
-        validators: [CustomValidators.dateRange],
-      }
-    );
+      this.periodForm = this.builder.group(
+        {
+          filterStart: new FormControl(obsStartDate.startDate, Validators.required),
+          filterEnd: new FormControl(obsEndDate.endDate, Validators.required),
+        },
+        {
+          validators: [CustomValidators.dateRange],
+        }
+      );
 
     this.periodForm.valueChanges.subscribe(form => {
       this.obsStartDate.startDate = form.filterStart;
