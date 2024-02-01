a9cdb04020bed6dce0d5fba76262caba231497d0
Ribeiro <alexfariakof@gmail.com>
2024-02-01 17:00:56 -0300

    Manutenção na descontinuação do uso de toPromise() pelo RxJS
    https://angularindepth.com/posts/1287/rxjs-heads-up-topromise-is-being-deprecated

--- HomeBrokerSPA/HomeBrokerChart/src/app/shared/services/charts/chart.service.ts
+++ HomeBrokerSPA/HomeBrokerChart/src/app/shared/services/charts/chart.service.ts
@@ -1,3 +1,4 @@
+import { lastValueFrom } from 'rxjs';
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Dayjs } from "dayjs";
@@ -13,22 +14,27 @@ export class ChartService {
   }
 
   async get(startDate:Dayjs| string, endDate: Dayjs | string): Promise<IMagazineLuizaHistoryPrice[]> {
-    const result:IMagazineLuizaHistoryPrice[] = await this.http.get<any>(`${ this.routeUrl }/${startDate}/${endDate}`).toPromise();
+    const magazineLuizaHistoryPrice$ = this.http.get<any>(`${ this.routeUrl }/${startDate}/${endDate}`);
+    const result = await lastValueFrom(magazineLuizaHistoryPrice$) as IMagazineLuizaHistoryPrice[];
     return result;
   }
 
   async getSMA(startDate:Dayjs| string, endDate: Dayjs | string): Promise<any> {
-    const result = await this.http.get<any>(`${ this.routeUrl }/getsma/${startDate}/${endDate}`).toPromise();
+    const getSMA$ = this.http.get<any>(`${ this.routeUrl }/getsma/${startDate}/${endDate}`);
+    const result = await lastValueFrom(getSMA$);
     return result;
   }
 
   async getEMA(periodDays: number, startDate:Dayjs| string, endDate: Dayjs | string): Promise<any> {
-    const result = await this.http.get<any>(`${ this.routeUrl }/getema/${periodDays}/${startDate}/${endDate}`).toPromise();
+    const getEMA$ = this.http.get<any>(`${ this.routeUrl }/getema/${periodDays}/${startDate}/${endDate}`);
+    const result = await lastValueFrom(getEMA$);
     return result;
+
   }
 
   async getMACD(startDate:Dayjs| string, endDate: Dayjs | string): Promise<any> {
-    const result = await this.http.get<any>(`${ this.routeUrl }/getmacd/${startDate}/${endDate}`).toPromise();
+    const getMACD$ = this.http.get<any>(`${ this.routeUrl }/getmacd/${startDate}/${endDate}`);
+    const result = await lastValueFrom(getMACD$);
     return result;
   }
 }
